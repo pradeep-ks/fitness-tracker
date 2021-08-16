8cf2b5dff5e8f0b42e13132276a7f72e7ecdbf77
Pradeep Kumar <pradeepkumar0509@gmail.com>
2021-08-16 16:35:29 +0530

    Updated Angular version to 11

--- src/app/training/next-training/next-training.component.spec.ts
+++ src/app/training/next-training/next-training.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NextTrainingComponent } from './next-training.component';
 
@@ -6,7 +6,7 @@ describe('NextTrainingComponent', () => {
   let component: NextTrainingComponent;
   let fixture: ComponentFixture<NextTrainingComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NextTrainingComponent ]
     })
--- src/app/training/previous-training/previous-training.component.spec.ts
+++ src/app/training/previous-training/previous-training.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PreviousTrainingComponent } from './previous-training.component';
 
@@ -6,7 +6,7 @@ describe('PreviousTrainingComponent', () => {
   let component: PreviousTrainingComponent;
   let fixture: ComponentFixture<PreviousTrainingComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PreviousTrainingComponent ]
     })
