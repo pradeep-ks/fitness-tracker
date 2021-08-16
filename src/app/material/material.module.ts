db4fe562fdecb31c93217c5d7521812c93a42780
Pradeep Kumar <pradeepkumar0509@gmail.com>
2021-08-16 17:57:04 +0530

    Minor changes

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,17 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {
-  MatButtonModule,
-  MatIconModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatDatepickerModule,
-  MatNativeDateModule,
-  MatCheckboxModule,
-  MatSidenavModule,
-  MatToolbarModule,
-  MatListModule
-} from '@angular/material';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 const MaterialModulesList = [
   CommonModule,
@@ -24,11 +22,11 @@ const MaterialModulesList = [
   MatCheckboxModule,
   MatSidenavModule,
   MatToolbarModule,
-  MatListModule
+  MatListModule,
 ];
 
 @NgModule({
   imports: [MaterialModulesList],
-  exports: [MaterialModulesList]
+  exports: [MaterialModulesList],
 })
-export class MaterialModule { }
+export class MaterialModule {}
